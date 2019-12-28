ad0b848d809784534e80279be0dfa882216c7302
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-28 20:44:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -90,10 +90,10 @@ def movies_with_directors_set(movie_collection)
 
 #set up the loop to first isolate the director hash, then get the name and movie data from that hash
   while index < movie_collection.length do 
-    movies_by_director << movies_with_director_name(name[index], movie_collection)
-    
+    movies_by_director << movies_with_director_name(name[index], movie_data)
   index += 1
   end
+  
   movies_by_director
 end
  
